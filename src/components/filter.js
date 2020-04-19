18f41a5438d678510c3a0d36a8781170a4f2669a
ba1tazar4ik <ba1tazar4ik@gmail.com>
2020-04-19 17:22:56 +0300

    заменил map на reduce

--- src/components/filter.js
+++ src/components/filter.js
@@ -15,7 +15,7 @@ const createFilterMarkup = (filter, isChecked) => {
 };
 
 export const createFilterTemplate = (filters) => {
-  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
+  const filtersMarkup = filters.reduce((currentValue, index) => createFilterMarkup((currentValue, index === 0)), ``);
 
   return (
     `<section class="main__filter filter container">
